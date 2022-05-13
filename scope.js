d8859d9efc9cbe3b2506d4e6a712d9dd0bdedee0
halil <halilerk77@icloud.com>
2022-05-13 19:13:31 +0300

    last-version

---
+++ scope.js
@@ -0,0 +1,47 @@
+// {
+//   var x = 2;
+// }
+// // x CAN BE used here
+
+// {
+//   let x = 2;
+// }
+// // x CAN NOT be used here
+
+let x = 10;
+var q = 100;
+const e = 1000;
+
+{
+  let x = 20;
+  console.log(x);
+  var q = 200;
+  console.log(q);
+  const e = 2000;
+  console.log(e);
+}
+console.log(x);
+console.log(q);
+console.log(e);
+
+//! FONKSİYONUN İÇİNDE OLAN FONKSİYONUN İÇİNDE ÖLÜR
+let sum = 0;
+var memo = 1;
+function topla(a, b) {
+  var memo = 3 + 5;
+  let selman = a + b;
+  console.log(memo);
+  return selman;
+}
+console.log(topla(3, 5));
+console.log(memo);
+
+var sayi1 = 1000000000;
+let sayi2 = 10;
+{
+  let sayi2 = 2000;
+  console.log(sayi2);
+  var sayi1 = 20;
+}
+console.log(sayi1);
+console.log(sayi2);
