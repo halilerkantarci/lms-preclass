23aa85587e18ac50d97f8f7834a1d3a42bbd85bb
halil <halilerk77@icloud.com>
2022-05-19 21:03:50 +0300

    first lesson

--- objects.js
+++ objects.js
@@ -1,18 +1,58 @@
-//! Using an Object Literal
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 50,
-  eyeColor: "blue",
-};
-const person = {};
-person.firstName = "John";
-person.lastName = "Doe";
-person.age = 50;
-person.eyeColor = "blue";
-//! Using the JavaScript Keyword new
-const person = new Object();
-person.firstName = "John";
-person.lastName = "Doe";
-person.age = 50;
-person.eyeColor = "blue";
+// //! Using an Object Literal
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue",
+// };
+// const person = {};
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+// //! Using the JavaScript Keyword new
+// const person = new Object();
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+
+//! JavaScript Objects are Mutable ***********
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue",
+// };
+
+// const x = person;
+// x.age = 10; // Will change both x.age and person.age
+
+// console.log(person);
+// console.log(x);
+
+//! JavaScript Properties
+//! Accessing JavaScript Properties
+//* objectName.property      // person.age
+//* objectName["property"]   // person["age"]
+//* objectName[expression]   // x = "age"; person[x]
+
+// const person = {
+//   firstname: "John",
+//   lastname: "Doe",
+//   age: 50,
+//   eyecolor: "blue",
+// };
+
+// document.getElementById("demo").innerHTML =
+//   person.firstname + " is " + person.age + " years old.";
+
+// const person = {
+//   firstname: "John",
+//   lastname: "Doe",
+//   age: 50,
+//   eyecolor: "blue",
+// };
+
+// document.getElementById("demo").innerHTML =
+//   person["firstname"] + " is " + person["age"] + " years old.";
