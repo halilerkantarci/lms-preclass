0977d189fcee25bde67286407690e6c278667d85
halil <halilerk77@icloud.com>
2022-05-19 20:47:30 +0300

    objects

---
+++ objects.js
@@ -0,0 +1,18 @@
+//! Using an Object Literal
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+const person = {};
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+//! Using the JavaScript Keyword new
+const person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
