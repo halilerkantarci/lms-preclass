70b5b4b935fa701de42bcad0190b06579ae36f6a
halil <halilerk77@icloud.com>
2022-05-23 19:10:39 +0300

    arrays

--- arrays.js
+++ arrays.js
@@ -126,3 +126,72 @@ console.log(lang.slice(2)); //? 2. indeksten sonrasını al getir. 2. indeksi da
 console.log(lang.slice(2, 4)); //? 2. indeksi al 4.indeski alma
 
 //!Finding Max and Min Values in an Array
+
+//! JavaScript Sorting Arrays
+const frui = ["Banana", "Orange", "Apple", "Mango"];
+console.log(frui.sort()); // orijinali değişir
+console.log(frui.reverse()); // orijinali değişir
+//! Numeric Sort
+const points13 = [40, 100, 1, 5, 25, 10];
+points13.sort(function (a, b) {
+  return a - b;
+});
+console.log(points13);
+
+//! Sorting an Array in Random Order
+points13.sort(function (a, b) {
+  return 0.5 - Math.random();
+});
+console.log(points13);
+
+//! Using Math.max() on an Array
+function myArrayMax(arr) {
+  return Math.max.apply(null, arr);
+}
+console.log(myArrayMax(points13));
+
+//! Using Math.min() on an Array
+function myArrayMin(arr) {
+  return Math.min.apply(null, arr);
+}
+console.log(myArrayMin(points13));
+
+//* =====================================
+//*        JavaScript ITERATION
+//* =====================================
+
+//! JavaScript Array forEach()
+// The forEach() method calls a function (a callback function) once for each array element.
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+numbers.forEach(myFunction1);
+
+function myFunction1(value) {
+  return (txt += value + " ");
+}
+console.log(typeof txt, txt);
+
+//! JavaScript Array map()
+// The map() method creates a new array by performing a function on each array element.
+
+// The map() method does not execute the function for array elements without values.
+
+// The map() method does not change the original array.
+const numbers1 = [45, 4, 9, 16, 25];
+const numbers2 = numbers1.map(myFunction);
+
+function myFunction(value) {
+  return value * 2;
+}
+console.log(numbers2);
+
+//! JavaScript Array filter()
+// The filter() method creates a new array with array elements that passes a test.
+
+// This example creates a new array from elements with a value larger than 18:
+const numbers = [45, 4, 9, 16, 25];
+const over18 = numbers.filter(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
