869f92d6e86750572a25636ab9a02f9e69b21278
halil <halilerk77@icloud.com>
2022-05-25 13:57:39 +0300

    mean-median

--- Js-iterations/soru3.js
+++ Js-iterations/soru3.js
@@ -2,3 +2,13 @@
 // { mean: 38.5, median: 32 }
 
 const input = [12, 46, 32, 64];
+let mean = input.sort((a, b) => a - b).reduce((a, b) => a + b) / input.length;
+console.log(mean);
+if (input.length % 2 == 0) {
+  //   input.sort((a, b) => a - b);
+  let z = (input[input.length / 2] + input[input.length / 2 - 1]) / 2;
+  console.log(z);
+} else {
+  let v = input[Math.floor(input.length / 2)];
+  console.log(v);
+}
